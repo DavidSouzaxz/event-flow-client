c98be2290ec4ce246001aa70a940b0c25915a388
David <daviddavi1221@gmail.com>
2026-01-15 11:09:10 -0300

    fix: esqueci

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import api from "../services/api";
 
 export function Register() {
   const [formData, setFormData] = useState({
