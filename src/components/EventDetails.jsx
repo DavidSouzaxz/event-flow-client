3c1479fdee81a8274a838b66701de7ee70720382
DavidSouzaxz <daviddavi1221@gmail.com>
2026-01-19 10:19:47 -0300

    fix: ajusted page event

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Loader2 } from "lucide-react";
 import { useAuth } from "../context/AuthContext";
+import { ChevronLeft, Calendar, MapPin, User, ShieldCheck } from "lucide-react";
 
 import api from "../services/api";
 
